3b0fe49ca0987dea6b8c7521c5dc8165c5c9964d
sestriel <felix.tzschucke@gmail.com>
2025-02-01 16:41:07 +0100

    Implemented Callback, fixed lint errors

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["admin/blockly.js", "**/.eslintrc.js", "admin/words.js", "node_modules", ".*"],
+}, ...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+
+    rules: {
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        "no-console": "off",
+        "no-var": "error",
+        "prefer-const": "error",
+
+        quotes: ["error", "double", {
+            avoidEscape: true,
+            allowTemplateLiterals: true,
+        }],
+
+        semi: ["error", "always"],
+    },
+}];
